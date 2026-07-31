90678e3c6c6ee9727a2a18250ba9c4de00f091d4
syazzxop-sketch <syazzxop@gmail.com>
2026-07-31 12:41:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8951981980:AAHHSrhbH0c-hEBAqUe4iTbooyh6CxnUkGM")
-UPI_ID = os.getenv("UPI_ID", "mikacswinpay-1@oksbi")
-SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME", "@miss_AnjaliWS")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "https://t.me/+b3fW6Gkqpv9kOTh1")
+# Bot Token
+BOT_TOKEN = os.getenv(
+    "BOT_TOKEN",
+    "8951981980:AAHHSrhbH0c-hEBAqUe4iTbooyh6CxnUkGM"
+)
 
+# App Name
 APP_NAME = "WinPay"
+
+# UPI ID
+UPI_ID = "mikacswinpay-1@oksbi"
+
+# Customer Support
+SUPPORT_USERNAME = "@miss_AnjaliWS"
+
+# Official Channel
+CHANNEL_USERNAME = "https://t.me/+b3fW6Gkqpv9kOTh1"
