dec5a8fb1995403ec8461ff4ee9c66cdf5af4a1c
syazzxop-sketch <syazzxop@gmail.com>
2026-07-31 11:31:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv(8951981980:AAGMDS1tSvc8K8hqmKPGo4BAWcniYWIszwk, "")
+UPI_ID = os.getenv("UPI_ID", mikacswinpay-1@oksbi)
+SUPPORT_USERNAME = os.getenv("", @miss_AnjaliWS)
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "https://t.me/+b3fW6Gkqpv9kOThl")
+APP_NAME = "WinPay"
