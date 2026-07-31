f202ba88e64acf533614716a159372cd589ebf1d
syazzxop-sketch <syazzxop@gmail.com>
2026-07-31 11:37:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8951981980:AAGMDS1tSvc8K8hqmKPGo4BAWcniYWIszwk")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8951981980:AAHHSrhbH0c-hEBAqUe4iTbooyh6CxnUkGM")
 UPI_ID = os.getenv("UPI_ID", "mikacswinpay-1@oksbi")
 SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME", "@miss_AnjaliWS")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "https://t.me/+b3fW6Gkqpv9kOTh1")
