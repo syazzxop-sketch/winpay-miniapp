a64e2b784b6244aebc58d6a71e5b54f497bf5818
syazzxop-sketch <syazzxop@gmail.com>
2026-07-31 15:02:09 +0530

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,13 +1,39 @@
 from telegram import ReplyKeyboardMarkup
 
+# Main Menu
 def main_menu():
     keyboard = [
         ["⚡ Deposit (UPI)", "🎁 Bonus Offers"],
         ["💎 Premium", "📖 How To Deposit"],
         ["🎧 Customer Support"]
     ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-    return ReplyKeyboardMarkup(
-        keyboard,
-        resize_keyboard=True
-    )
+
+# Deposit Amount Menu
+def deposit_menu():
+    keyboard = [
+        ["💰 ₹100", "💰 ₹200"],
+        ["💰 ₹500", "💰 ₹1000"],
+        ["💰 ₹2000", "💰 ₹5000"],
+        ["⬅️ Back"]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
+# Payment Menu
+def payment_menu():
+    keyboard = [
+        ["📤 Send Screenshot"],
+        ["🎧 Customer Support"],
+        ["⬅️ Back"]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
+# Bonus Menu
+def bonus_menu():
+    keyboard = [
+        ["⬅️ Back"],
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
