f1999151e6c283ee9a1dd2e5ac39fc7978b5b966
syazzxop-sketch <syazzxop@gmail.com>
2026-07-31 13:49:01 +0530

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,62 @@
+WELCOME_MESSAGE = """
+╔════════════════════╗
+        🎉 𝗪𝗜𝗡𝗣𝗔𝗬 🎉
+╚════════════════════╝
+
+✨ *Welcome to WinPay*
+
+💯 Trusted Platform
+⚡ Fast Service
+🔒 Secure Payments
+
+━━━━━━━━━━━━━━━━━━
+
+🔑 *Please Enter Your Personal Invitation Code.*
+
+━━━━━━━━━━━━━━━━━━
+"""
+
+SUCCESS_MESSAGE = """
+╔════════════════════╗
+        ✅ 𝗦𝗨𝗖𝗖𝗘𝗦𝗦
+╚════════════════════╝
+
+🎉 Congratulations!
+
+Your Invitation Code Has Been Submitted Successfully.
+
+💎 Welcome to *WinPay*
+
+━━━━━━━━━━━━━━━━━━
+
+👇 Choose an option below.
+"""
+
+DEPOSIT_MESSAGE = """
+⚡ *OFFLINE UPI DEPOSIT*
+
+━━━━━━━━━━━━━━━━━━
+
+🏦 UPI ID
+
+`winpay@upi`
+
+━━━━━━━━━━━━━━━━━━
+
+📌 Deposit Offers
+
+✅ ₹500  ➜ 🎁 ₹625
+✅ ₹1000 ➜ 🎁 ₹1356
+✅ ₹1500 ➜ 🎁 ₹1751
+✅ ₹2000 ➜ 🎁 ₹2411
+✅ ₹2500 ➜ 🎁 ₹2806
+✅ ₹3000 ➜ 🎁 ₹3454
+✅ ₹3500 ➜ 🎁 ₹4009
+✅ ₹4000 ➜ 🎁 ₹4563
+✅ ₹4500 ➜ 🎁 ₹4987
+✅ ₹5000 ➜ 🎁 ₹5521
+
+━━━━━━━━━━━━━━━━━━
+
+📸 Send Screenshot to Customer Support 🎧
+"""
