67f484456098347d88e9fa6483ccaebd2feab65d
syazzxop-sketch <syazzxop@gmail.com>
2026-07-31 13:35:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8951981980:AAHHSrhbH0c-hEBAqUe4iTbooyh6CxnUkGM"
+
+UPI_ID = "mikacswinpay-1@oksbi"
+
+CHANNEL_USERNAME = "https://t.me/+b3fW6Gkqpv9kOThl"
+
+SUPPORT_USERNAME = "@miss_AnjaliWS"
